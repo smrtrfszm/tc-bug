9e4913cce5e3751470325a54f13c6c81fd4f5f9f
Szepesi Tibor <smrtrfszm@proton.me>
2024-08-12 17:01:57 +0200

    add another container

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,13 @@ use testcontainers_modules::postgres::Postgres;
 async fn main() {
     for i in 0..50 {
         println!("{i}");
-        let container = Postgres::default()
+        let container2 = Postgres::default()
+            .with_tag("16")
+            .start()
+            .await
+            .unwrap();
+
+        let container1 = Postgres::default()
             .with_tag("16")
             .start()
             .await
@@ -14,13 +20,16 @@ async fn main() {
 
         let connection_string = &format!(
             "postgres://postgres:postgres@{}:{}/postgres",
-            container.get_host().await.unwrap(),
-            container.get_host_port_ipv4(5432).await.unwrap(),
+            container1.get_host().await.unwrap(),
+            container1.get_host_port_ipv4(5432).await.unwrap(),
         );
 
 
         let pool = PgPool::connect(connection_string).await.unwrap();
 
         sqlx::query("select 1").execute(&pool).await.unwrap();
+
+        drop(container1);
+        drop(container2);
     }
 }
